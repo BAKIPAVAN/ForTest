5971f3c645d94e44955dfc8fb25269b5d90e6b3c
BAKIPAVAN <DarkAngle@23$>
2024-10-20 21:19:09 +0530

    done

---
+++ src/main/java/com/example/demo/BuildDeploymenyApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildDeploymenyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildDeploymenyApplication.class, args);
+		System.out.println("Started.......");
+	}
+
+}
